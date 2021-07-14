cbba20055c46daaadca6b2ca1d59b589a14e74c2
rtmigo <art.galkin@gmail.com>
2021-07-14 03:39:44 +0300

    upd

--- test_pkg.py
+++ test_pkg.py
@@ -2,6 +2,6 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('import fast_file_hash')
+        pkg.run_python_code('import coarse_hash')
 
     print("\nPackage is OK!")
